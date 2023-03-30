ebdf68bb93546ea6aebb7fba2e703fb70a52c7db
rafailka-m <rafayt323@gmail.com>
2023-03-30 19:33:36 +0300

    feat(randomNumber): get request to get random card

--- src/limit_parser/app/viewsets.py
+++ src/limit_parser/app/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import action
 from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from app import models, serializers
@@ -9,6 +10,14 @@ class FuelCardViewSet(ModelViewSet):
 
     queryset = models.FuelCard.objects.all()
     serializer_class = serializers.FuelCardSerializer
+
+    @action(detail=False,
+            url_path=r'random')
+    def get_random_number(self, _: Request) -> Response:
+        filtered_numbers = models.FuelCard.objects.filter(is_took=False)
+        random_number = filtered_numbers.order_by('?')[0]
+        serializer = self.get_serializer(random_number)
+        return Response(serializer.data)
 
     @action(detail=False,
             methods=['put'],
